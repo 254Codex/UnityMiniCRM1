09f0875dcc2c99c59dda95d57cf97a7c298532b5
254Codex <wanyoikebramwel4@gmail.com>
2026-01-15 15:57:33 +0300

    Add migration script for existing data transfer
    This migration script is designed to optionally transfer data from old models, but currently indicates that no existing data is present to migrate.

--- backend/tasks/migrations/0003_migrate_existing_data.py
+++ backend/tasks/migrations/0003_migrate_existing_data.py
@@ -1,1 +1,30 @@
 
+# backend/tasks/migrations/0003_migrate_existing_data.py
+
+from django.db import migrations
+import uuid
+from django.utils import timezone
+
+
+def migrate_existing_data(apps, schema_editor):
+    """
+    This is an optional migration to transfer data from old models if they exist.
+    Since you're replacing models, you likely don't have data to migrate.
+    """
+    print("No existing data to migrate. Starting fresh with enhanced models.")
+
+
+def reverse_migration(apps, schema_editor):
+    """Reverse migration - nothing to do since we're not keeping old data"""
+    pass
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tasks', '0002_supporting_models'),
+    ]
+
+    operations = [
+        migrations.RunPython(migrate_existing_data, reverse_migration),
+    ]
